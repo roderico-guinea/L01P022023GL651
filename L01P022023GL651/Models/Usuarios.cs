f47a0d07ea7be002be1230823791aab0b4b3aa5e
Roderico Guinea <guinearoderico2@gmail.com>
2025-03-28 17:13:50 -0600

    Creacion de Modelo Usuarios

---
+++ L01P022023GL651/Models/Usuarios.cs
@@ -0,0 +1,14 @@
+﻿namespace L01P022023GL651.Models
+{
+    public class Usuarios
+
+    {
+        public int usuarioId { get; set; }
+        public string? usuarioNombre { get; set; }
+        public string? usuarioApellido { get; set; }
+        public string? usuarioCorreo { get; set; }
+        public string? usuarioContrasena { get; set; }
+        public int rolId { get; set; }
+        public Rol? Rol { get; set; }
+    }
+}
