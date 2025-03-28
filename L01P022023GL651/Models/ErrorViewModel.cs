24baea1c4b84bc98f94d6c3f1a56c58c7925157b
Roderico Guinea <guinearoderico2@gmail.com>
2025-03-28 17:02:16 -0600

    Agregar archivos de proyecto.

---
+++ L01P022023GL651/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L01P022023GL651.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
