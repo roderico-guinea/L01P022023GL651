902799e70abf5e9eda939a23382200860e799b5a
Roderico Guinea <guinearoderico2@gmail.com>
2025-03-28 17:14:41 -0600

    Creacion de Modelo Publicaciones

---
+++ L01P022023GL651/Models/Publicaciones.cs
@@ -0,0 +1,12 @@
+﻿namespace L01P022023GL651.Models
+{
+    public class Publicaciones
+
+    {
+        public int publicacionId { get; set; }
+        public string? publicacionTitulo { get; set; }
+        public string? publicacionContenido { get; set; }
+        public int usuarioId { get; set; }
+        public Usuarios? Usuario { get; set; }
+    }
+}
