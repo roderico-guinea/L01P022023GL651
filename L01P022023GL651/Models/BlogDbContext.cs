12102c9bd72de83139da3835d449dbd9b9d668d7
Roderico Guinea <guinearoderico2@gmail.com>
2025-03-28 17:30:09 -0600

    Creacion de Clase blogDbContext

---
+++ L01P022023GL651/Models/BlogDbContext.cs
@@ -0,0 +1,16 @@
+﻿using L01P022023GL651.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace L01P022023GL651.Models
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options) { }
+
+        public DbSet<Rol> Roles { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Publicacion> Publicaciones { get; set; }
+        public DbSet<Comentario> Comentarios { get; set; }
+        public DbSet<Calificacion> Calificaciones { get; set; }
+    }
+}
