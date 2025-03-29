c8feba2c67d7eac5797da201a7980afc51423ffc
Roderico Guinea <guinearoderico2@gmail.com>
2025-03-28 18:21:12 -0600

    Creacion de vistas

---
+++ L01P022023GL651/Views/Comentarios/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace L01P022023GL651.Views.Comentarios
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ L01P022023GL651/Views/Roles/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace L01P022023GL651.Views.Roles
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
